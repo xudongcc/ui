af47c2629b39c33ecceb1aa43bb063bef4c82889
Xudong Huang <me@xudong.cc>
2024-01-04 03:04:19 +0800

    fix: Update build configuration and package.json paths

--- modern.config.ts
+++ modern.config.ts
@@ -1,36 +1,6 @@
-import {
-  PartialBuildConfig,
-  defineConfig,
-  moduleTools,
-} from "@modern-js/module-tools";
-
-const commonBuildConfig: PartialBuildConfig = {
-  buildType: "bundleless",
-  input: ["src", "!src/**/*.stories.tsx"],
-  outDir: "./dist",
-  dts: false,
-};
+import { defineConfig, moduleTools } from "@modern-js/module-tools";
 
 export default defineConfig({
   plugins: [moduleTools()],
-  buildConfig: [
-    {
-      ...commonBuildConfig,
-      format: "cjs",
-      target: "es6",
-      autoExtension: true,
-    },
-    {
-      ...commonBuildConfig,
-      format: "esm",
-      target: "es6",
-      autoExtension: true,
-    },
-    {
-      ...commonBuildConfig,
-      dts: {
-        only: true,
-      },
-    },
-  ],
+  buildPreset: "npm-component",
 });
