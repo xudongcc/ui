24094ef4f18ce1f5aa54bf8f6a2e8e5f7c88aeec
Xudong Huang <me@xudong.cc>
2024-01-05 13:57:12 +0800

    fix: exclude *.stories.tsx

--- modern.config.ts
+++ modern.config.ts
@@ -1,6 +1,37 @@
-import { defineConfig, moduleTools } from "@modern-js/module-tools";
+import {
+  defineConfig,
+  moduleTools,
+  type PartialBuildConfig,
+} from "@modern-js/module-tools";
+
+const commonBuildConfig: PartialBuildConfig = {
+  buildType: "bundleless",
+  input: ["src", "!**/*.stories.*"],
+  dts: false,
+};
 
 export default defineConfig({
   plugins: [moduleTools()],
-  buildPreset: "npm-component",
+  buildConfig: [
+    {
+      ...commonBuildConfig,
+      format: "cjs",
+      target: "es6",
+      outDir: "./dist/lib",
+    },
+    {
+      ...commonBuildConfig,
+      format: "esm",
+      target: "es6",
+      outDir: "./dist/es",
+    },
+    {
+      ...commonBuildConfig,
+      outDir: "./dist/types",
+      dts: {
+        only: true,
+        tsconfigPath: "./tsconfig.build.json",
+      },
+    },
+  ],
 });
